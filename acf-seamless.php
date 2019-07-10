99e5176117b1512e8f7aa9b9bd6cdc514b35e9a6
Chris Yee <christophery@gmail.com>
2019-07-10 18:58:15 -0400

    Removed $hook parameter

--- acf-seamless.php
+++ acf-seamless.php
@@ -13,7 +13,7 @@ Text Domain: cmyee-acf-seamless
  * Register JS
  */
 
-function cmyee_acf_seamless_js( $hook ) {
+function cmyee_acf_seamless_js() {
     wp_enqueue_script( 'cmyee-acf-seamless', plugins_url( 'js/acf-seamless.js', __FILE__ ) );
 }
 
